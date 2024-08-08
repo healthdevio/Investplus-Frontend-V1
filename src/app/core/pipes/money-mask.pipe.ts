ad4300c42f43cc1beb2ec469764717823356922d
“odevnicolas” <nickolas@s4s.tech>
2024-08-08 12:00:02 -0300

    mais alguams atualizacoes

--- src/app/core/pipes/money-mask.pipe.ts
+++ src/app/core/pipes/money-mask.pipe.ts
@@ -1,9 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'moneyMask'})
+@Pipe({ name: 'moneyMask' })
 export class MoneyMaskPipe implements PipeTransform {
   transform(value: number = 0, sifrao = false): string {
     let dinheiro = value ? value : 0;
-    return dinheiro.toLocaleString('pt-br', !sifrao ? { minimumFractionDigits: 2 } : { style: 'currency', currency: 'BRL' });
+    let formattedValue = dinheiro.toLocaleString('pt-br', { minimumFractionDigits: 2 });
+    return sifrao ? `R$ ${formattedValue}` : formattedValue;
   }
 }
