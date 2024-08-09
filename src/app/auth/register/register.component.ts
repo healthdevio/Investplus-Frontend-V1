37e89ff60958d45ccb0e3bad965d8119b5c9c91f
“odevnicolas” <nickolas@s4s.tech>
2024-08-09 10:35:15 -0300

    atualizacao de termos de uso

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -225,7 +225,15 @@ export class RegisterComponent implements OnInit {
   onSubmit() {
     this.loader = true;
     this.loaderService.load(this.loader);
-    if (this.form.valid && this.form.value.termosGerais === true && this.form.value.typeDocument !== null) {
+
+    if (!this.form.value.termosGerais) {
+      toastr.error('Você deve concordar com os termos de uso.');
+      this.loader = false;
+      this.loaderService.load(this.loader);
+      return;
+    }
+
+    if (this.form.valid && this.form.value.typeDocument !== null) {
       const dataSend = this.form.value;
       dataSend.termosGerais = undefined;
       dataSend.typeDocument = undefined;
