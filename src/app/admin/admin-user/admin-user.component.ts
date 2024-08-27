6b6b3838c0ff66c51a50cb53e02208962307f870
odevnicolas <odevnicolas@gmail.com>
2024-08-27 10:14:07 -0300

    wip: my profile

--- src/app/admin/admin-user/admin-user.component.ts
+++ src/app/admin/admin-user/admin-user.component.ts
@@ -340,6 +340,28 @@ private setFormValue(controlName: string, value: any) {
     }
   }
 
+  onDevelopmentToast() {
+    toastr.options = {
+      closeButton: true,
+      debug: false,
+      newestOnTop: false,
+      progressBar: true,
+      positionClass: "toast-top-right",
+      preventDuplicates: true,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "10000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
+
+    toastr.error('Em desenvolvimento');
+  }
+
   redirectTo(uri: string) {
     this.router
       .navigateByUrl("/", {
