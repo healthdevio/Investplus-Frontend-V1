f7576bb730ef66d559877bbbe3846028e72a8165
odevnicolas <odevnicolas@gmail.com>
2024-09-25 13:12:35 -0300

    feat: cut update sessions

--- src/app/admin/admin-left-side/admin-left-side.component.ts
+++ src/app/admin/admin-left-side/admin-left-side.component.ts
@@ -37,24 +37,8 @@ export class AdminLeftSideComponent implements OnInit {
       link: null,
       subCategory: [
         {
-          name: 'Dados gerais',
-          link: '/admin/rounds/approval',
-        },
-        {
-          name: 'Valutation',
-          link: '/admin/rounds/company/valuation',
-        },
-        {
           name: 'Captable',
           link: '/admin/rounds/company/captable',
-        },
-        {
-          name: 'Receita e despesa',
-          link: '/admin/rounds/company/financial',
-        },
-        {
-          name: 'Administradores',
-          link: '/admin/rounds/company/admin',
         },
         {
           name: 'Equipe Executiva',
