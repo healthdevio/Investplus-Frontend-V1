a0f2af81f187beafe2838818d3490f723c704cac
odevnicolas <odevnicolas@gmail.com>
2024-09-30 10:18:16 -0300

    wip: singUpPublish

--- src/app/admin/admin-left-side/admin-left-side.component.ts
+++ src/app/admin/admin-left-side/admin-left-side.component.ts
@@ -37,11 +37,6 @@ export class AdminLeftSideComponent implements OnInit {
 
   importatedSubItens = [
     {
-      name: 'Criar Rodada',
-      link: '/admin/rounds/incorporator/create',
-      isExpanded: false
-    },
-    {
       name: 'Publicar Rodada',
       link: '/admin/rounds/approval/incorporator/publish',
       isExpanded: false
