d7e6f9dd8e59c990749fd178b8a812358d867d87
xXNextGenXx <mundocurioso3294@gmail.com>
2024-11-05 21:10:01 -0300

    ajuste do env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -21,8 +21,8 @@ export const environment = {
   s3_endpoint: "", */
   production: true,
   // url_api: "https://api.fcjinvest.com.br/api",
-  // url_api: "https://homologinvestplus.investplus.vc/api",
-  url_api: "http://localhost:8080/api",
+  url_api: "https://homologinvestplus.investplus.vc/api",
+  // url_api: "http://localhost:8080/api",
   userPoolId: "us-east-2_GAF4pxPcD",
   clientId: "3ph8jpuh1f6maiuilank50gbfe",
   region: "us-east-2",
